940159b58e92dd6312bdcab242707e3bfa378a06
Yanis <yanislapo@gmail.com>
2022-04-01 09:34:33 +0200

    topbar

---
+++ client/src/js/topbar.js
@@ -0,0 +1,9 @@
+// get the file name from the url
+const getFileName = () => {
+    const url = window.location.href;
+    const parts = url.split('/');
+    const filename = parts[parts.length - 1];
+    return filename;
+}
+
+document.querySelector(`a[href='/${getFileName()}']`).classList.add("active");
