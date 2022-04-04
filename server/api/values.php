0fd511a0376c3c7dad28a48e35c2ece58963870a
Yanis <yanislapo@gmail.com>
2022-04-04 10:59:36 +0200

    fix post values

--- server/api/values.php
+++ server/api/values.php
@@ -5,56 +5,30 @@ include('../includes/functions.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $_POST = jsonInput();
-    var_dump($_POST);
+    $res = [];
 
-    $data;
-    $i = 0;
     foreach ($_POST as $key => $value) {
-        // if (!is_numeric($key)) {
-        $data[$i][$key] = $value;
-        // }
+        if (is_numeric($key)) {
+            // echo json_encode(array(
+            //     "post" => $_POST,
+            //     "key" => $key,
+            //     "value" => $value
+            // ));
+
+            $req = $bdd->prepare('INSERT INTO sensorsValues (balise_id, sensor_id, value) VALUES (:balise_id, :sensor_id, :value)');
+            $req->execute(array(
+                "balise_id" => $_POST['balise_id'],
+                "sensor_id" => $key,
+                "value" => $value
+            ));
+
+            $res[] = array(
+                "errorInfo" => $req->errorInfo()
+            );
+        }
     }
-    $i++;
-
-    var_dump(json_encode($data));
-
-    // $req = $bdd->prepare('SELECT * FROM listBalise WHERE balise_id = :balise_id');
-    // $req->execute(array(
-    //     "balise_id" => $_POST['balise_id']
-    // ));
-
-    // $data;
-    // $i = 0;
-    // while ($req_f = $req->fetch()) {
-    //     foreach ($req_f as $key => $value) {
-    //         if (!is_numeric($key)) {
-    //             if ($key == 'sensors_id') {
-    //                 $data[$i][$key] = explode(',', $value);
-    //             } else {
-    //                 $data[$i][$key] = $value;
-    //             }
-    //         }
-    //     }
-    //     $i++;
-    // }
-    // var_dump($data);
-
-    // $req = $bdd->prepare('SELECT * FROM listSensors WHERE sensors_id = :sensors_id AND ');
-
-    // $req = $bdd->prepare('SELECT * FROM listSensors WHERE name LIKE "%:name%"')
-    // $req->execute(array(
-    //     "name" => 
-    // ))
-
-    // $req = $bdd->prepare('INSERT INTO sensorsValues (balise_id, sensor_id, value) VALUES (:balise_id, SELECT * FROM listSensors WHERE name LIKE "%:name%"', :value)');
-    // $req->execute(array(
-    //     "balise_id" => $_POST['balise_id'],
-    //     "name" => "te,
-    //     "value" => $_POST['value']
-    // ));
-
     header('Content-Type: application/json');
-    // echo json_encode(array("errorInfo" => $req->errorInfo()));
+    echo json_encode($res);
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
