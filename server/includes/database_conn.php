32b513dacf1904333c257d69cd3bdbbf74fbbe21
Kosmo <yanislapo@gmail.com>
2022-04-13 00:28:25 +0200

    added docker

--- server/includes/database_conn.php
+++ server/includes/database_conn.php
@@ -1,4 +1,8 @@
 <?php
 date_default_timezone_set('Europe/Paris');
 
-$bdd = new PDO('mysql:host=192.168.0.120;dbname=monitoring_pesquiers;charset=utf8', 'dbeaver', 'dbeaver'); // For getting token
+try {
+    $bdd = new PDO('mysql:host=db;dbname=monitoring_pesquiers;charset=utf8', 'root', 'root');
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
