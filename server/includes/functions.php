a822fe957c900eba91159a654e435518c55b463d
Kosmo <yanislapo@gmail.com>
2022-04-20 01:53:35 +0200

    adding patch and delete method

--- server/includes/functions.php
+++ server/includes/functions.php
@@ -49,7 +49,7 @@ function cors() {
 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
             // may also be using PUT, PATCH, HEAD etc
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+            header("Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS");
 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
             header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
