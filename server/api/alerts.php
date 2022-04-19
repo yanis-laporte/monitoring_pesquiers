c51855453b770709f51ea2449e9beae83dfa243b
Kosmo <yanislapo@gmail.com>
2022-04-20 01:53:19 +0200

    fix new database columns

--- server/api/alerts.php
+++ server/api/alerts.php
@@ -20,17 +20,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $name = $_POST['name'];
-    $template = $_POST['template'];
-    $balise_id = $_POST['balise_id'];
-    $sensor_id = $_POST['sensor_id'];
-
-    $req = $bdd->prepare('INSERT INTO alerts (name, template, balise_id, sensor_id) VALUES (:name, :template, :balise_id, :sensor_id)');
+    $req = $bdd->prepare('INSERT INTO alerts (name, template, balise_id, sensor_id, control, sign, email) VALUES (:name, :template, :balise_id, :sensor_id, :control, :sign, :email)');
     $req->execute(array(
-        "name" => $name,
-        "template" => $template,
-        "balise_id" => $balise_id,
-        "sensor_id" => $sensor_id
+        "name" => $_POST['name'],
+        "template" => $_POST['template'],
+        "balise_id" => $_POST['balise_id'],
+        "sensor_id" => $_POST['sensor_id'],
+        "control" => $_POST['control'],
+        "sign" => $_POST['sign'],
+        "email" => $_POST['email']
     ));
 
     header('Content-Type: application/json');
@@ -40,24 +38,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 if ($_SERVER['REQUEST_METHOD'] == 'PATCH') {
     $_PATCH = jsonInput();
 
-    $alert_id = $_PATCH['alert_id'];
-    $name = $_PATCH['name'];
-    $template = $_PATCH['template'];
-    $balise_id = $_PATCH['balise_id'];
-    $sensor_id = $_PATCH['sensor_id'];
-
     $req = $bdd->prepare('UPDATE alerts SET
         name = :name,
         template = :template,
         balise_id = :balise_id,
-        sensor_id =  :sensor_id
+        sensor_id =  :sensor_id,
+        control =  :control,
+        sign =  :sign,
+        email =  :email
         WHERE alert_id = :alert_id;');
     $req->execute(array(
-        "name" => $name,
-        "template" => $template,
-        "balise_id" => $balise_id,
-        "sensor_id" => $sensor_id,
-        "alert_id" => $alert_id
+        "alert_id" => $_PATCH['alert_id'],
+        "name" => $_PATCH['name'],
+        "template" => $_PATCH['template'],
+        "balise_id" => $_PATCH['balise_id'],
+        "sensor_id" => $_PATCH['sensor_id'],
+        "control" => $_PATCH['control'],
+        "sign" => $_PATCH['sign'],
+        "email" => $_PATCH['email']
     ));
 
     header('Content-Type: application/json');
@@ -67,11 +65,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'PATCH') {
 if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
     $_DELETE = jsonInput();
 
-    $alert_id = $_DELETE['alert_id'];
-
     $req = $bdd->prepare('DELETE FROM alerts WHERE alert_id = :alert_id');
     $req->execute(array(
-        "alert_id" => $alert_id
+        "alert_id" => $_DELETE['alert_id']
     ));
 
     header('Content-Type: application/json');
