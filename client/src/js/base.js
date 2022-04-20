86fafd1b644565c4a144c5c48c9bbe79b8cd5564
Kosmo <yanislapo@gmail.com>
2022-04-20 02:08:35 +0200

    jquery like function added

--- client/src/js/base.js
+++ client/src/js/base.js
@@ -1,3 +1,13 @@
 module.exports = {
-    API_URL: "http://127.0.0.1/edsa-pesquiers/api/"
+    // API_URL: "http://127.0.0.1/edsa-pesquiers/api/"
+    API_URL: "http://localhost/api",
+
+    /* A function that returns the element with the id passed as parameter. */
+    $: (e) => {
+        return document.getElementById(e)
+    },
+    /* A function that returns the elements with the name passed as parameter. */
+    $$: (e) => {
+        return document.getElementsByName(e)
+    }
 }
