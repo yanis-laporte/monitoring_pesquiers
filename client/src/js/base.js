0ab8c6bc69a58fb701bad5988695076e67791f24
Yanis <yanislapo@gmail.com>
2022-05-03 10:40:30 +0200

    rpi ip

--- client/src/js/base.js
+++ client/src/js/base.js
@@ -1,6 +1,7 @@
 module.exports = {
-    // API_URL: "http://127.0.0.1/edsa-pesquiers/api/"
-    API_URL: "http://localhost/api",
+    // API_URL: "http://127.0.0.1/edsa-pesquiers/api"
+    // API_URL: "http://localhost/api",
+    API_URL: "http://192.168.0.120/api",
 
     /* A function that returns the element with the id passed as parameter. */
     $: (e) => {
