0445734d308ad80d2f6ed551784032922fec1a78
Yanis <yanislapo@gmail.com>
2022-05-08 22:17:48 +0200

    drawing charts

--- client/src/js/charts.js
+++ client/src/js/charts.js
@@ -1,287 +1,276 @@
 import { API_URL, $, $$ } from './base';
+import { Toast } from './toast';
 
-// function addData(data) {
-//     for (var i = 0; i < data.length; i++) {
-//         dataPoints.push({
-//             x: new Date(data[i].date),
-//             y: data[i].units
-//         });
-//     }
-//     chart.render();
-
-// }
-
-// function drawChart(container_id) {
-//     var chart = new CanvasJS.Chart(container_id)
-// }
-
-
-async function fetchNode(b_id) {
-    return await fetch(`${API_URL}/nodes.php?id=${b_id}`)
+async function fetchNode(id) {
+    return await fetch(`${API_URL}/nodes.php?id=${id}`)
         .then(res => res.json())
 }
 
 async function fetchValues(id) {
-    await fetch(`${API_URL}/values.php?u=true&id=${id}`)
+    return await fetch(`${API_URL}/values.php?u=true&id=${id}`)
         .then(res => res.json())
-        .then(data => a = data)
-    return a
 }
 
 async function drawChart(data, units, container) {
-    units = units[0]
+    console.time('Drawing chart in')
+    console.debug('data:', data);
+    console.debug('units:', units);
+    console.debug('container:', container);
 
     let dataPoints = [];
-    let dataPointsY = data[0].map(e => parseInt(e.value));
-    let dataPointsX = data[0].map(e => new Date(e.timestamp).getTime());
+    let dataPointsY = data.map(e => parseInt(e.value));
+    let dataPointsX = data.map(e => new Date(e.timestamp).getTime());
     dataPointsY.forEach((e, i) => {
         // Value, Timestamp
         dataPoints.push([dataPointsX[i], dataPointsY[i],])
     });
 
+    Highcharts.setOptions({
+        lang: {
+            loading: 'Chargement...',
+            months: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+            weekdays: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+            shortMonths: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Août', 'Sept', 'Oct', 'Nov', 'Déc'],
+
+            rangeSelectorFrom: 'Du',
+            rangeSelectorTo: 'Au',
+            rangeSelectorZoom: 'Période',
+
+            exportButtonTitle: 'Exporter',
+            resetZoom: 'Réinitialiser le zoom',
+            resetZoomTitle: 'Réinitialiser le zoom',
+
+            viewFullscreen: 'Plein écran',
+            printChart: 'Imprimer le graphique',
+            downloadPNG: 'Télécharger en PNG',
+            downloadJPEG: 'Télécharger en JPEG',
+            downloadPDF: 'Télécharger en PDF',
+            downloadSVG: 'Télécharger en SVG',
+            downloadCSV: 'Exporter en CSV',
+            downloadXLS: 'Exporter au format Excel',
+            viewData: 'Voir la table de données',
+            contextButtonTitle: 'Menu contextuel'
+        },
+    });
+
     Highcharts.chart(container, {
+        // #region Chart options
+        // rangeSelector: {
+        //     enabled: true,
+        //     inputEnabled: false,
+        //     buttonPosition: {
+        //         align: 'right'
+        //     },
+        //     labelStyle: {
+        //         display: 'none'
+        //     },
+        //     buttons: [{
+        //         type: 'month',
+        //         count: 1,
+        //         text: '1m'
+        //     },
+        //     {
+        //         type: 'month',
+        //         count: 3,
+        //         text: '3m'
+        //     },
+        //     {
+        //         type: 'month',
+        //         count: 6,
+        //         text: '6m'
+        //     },
+        //     {
+        //         type: 'year',
+        //         count: 1,
+        //         text: '1y'
+        //     },
+        //     {
+        //         type: 'year',
+        //         count: 2,
+        //         text: '2y'
+        //     },
+        //     {
+        //         type: 'year',
+        //         count: 5,
+        //         text: '5y'
+        //     },
+        //     {
+        //         type: 'all',
+        //         text: 'All'
+        //     },
+        //     {
+        //         type: 'all',
+        //         text: 'Latest',
+        //         events: {
+        //             click: () => {
+
+        //                 alert('in real app I scroll to latest results');
+
+        //                 return false;
+
+        //             }
+        //         }
+        //     }]
+        // },
+        // navigator: {
+        //     enabled: true,
+        // },
+        // legend: {
+        //     enabled: false,
+        //     align: 'left',
+        //     verticalAlign: 'top',
+        //     borderWidth: 0
+        // },
+        // loading: {
+        //     // https://api.highcharts.com/highcharts/loading
+        // },
+
+        // tooltip: {
+        //     // https://api.highcharts.com/highcharts/tooltip
+        // },
+        // responsive: {
+        //     // https://api.highcharts.com/highcharts/responsive
+        //         rules: [{
+        //             condition: {
+        //                 maxWidth: 500
+        //             },
+        //             chartOptions: {
+        //                 legend: {
+        //                     layout: 'horizontal',
+        //                     align: 'center',
+        //                     verticalAlign: 'bottom'
+        //                 }
+        //             }
+        //         }]
+        //     }
+        // });
+        //#endregion
+
+        navigation: {
+            buttonOptions: {
+                theme: {
+                    // Good old text links
+                    // todo
+                    style: {
+                        color: '#039',
+                        // textDecoration: 'underline'
+                    }
+                }
+            }
+        },
+
+        exporting: {
+            buttons: {
+                contextButton: {
+                    enabled: true,
+                    menuItems: ["downloadPNG", "downloadJPEG", "downloadXLS", "separator", "printChart", "viewFullscreen"]
+                },
+                exportButton: {
+                    text: 'Télécharger',
+                    // Use only the download related menu items from the default
+                    // context button
+                    menuItems: [
+                        'downloadPNG',
+                        'downloadJPEG',
+                        'separator',
+                        'downloadXLS'
+                    ]
+                }
+            }
+        },
+
         chart: {
-            zoomType: 'x'
+            zoomType: 'x',
+            borderRadius: 5,
+            /* DEBUG */
+            backgroundColor: 'gray',
         },
         title: {
-            text: 'Titre'
+            text: `${units.name}`
         },
         subtitle: {
             text: document.ontouchstart === undefined ?
                 'Cliquer-glisser dans le zone de point pour agrandir' : 'Pincer le graphique pour zomer'
         },
+
         xAxis: {
-            type: 'datetime'
+            type: 'datetime',
+            // scrollbar: {
+            //     enabled: true
+            // },
         },
         yAxis: {
             title: {
-                text: 'Exchange rate'
+                text: `${units.name} en ${units.symb}`
             }
         },
-        legend: {
-            enabled: false
-        },
+
 
         series: [{
             type: 'line',
-            name: `${units.name}, ${units.unit}`,
+            name: `${units.unit} ${units.symb}`,
             data: dataPoints
         }]
     });
 
-    //#region 
-    // var chart = Highcharts.chart(container, {
-    //     title: {
-    //         text: `Évolution de la ${units.name} toute les 30 minutes`
-    //     },
-
-    //     yAxis: {
-    //         title: {
-    //             text: `Y axis`
-    //         }
-    //     },
-
-    //     xAxis: {
-    //         title: {
-    //             text: 'Date de X à Y FAIRE L\'DU TEMPS'
-    //         }
-    //     },
-
-    //     series: [{
-    //         yAxis: 0,
-    //         type: 'line',
-    //         name: 'Température',
-    //         data: dp
-    //     }],
-
-    // });
-
-
-    // https://www.highcharts.com/forum/viewtopic.php?t=35142
-
-    // var chart = Highcharts.chart(container, {
-
-    //     time: {
-    //         timezone: 'Europe/Paris'
-    //     },
-
-    //     lang: {
-    //         viewFullscreen: 'Plein écran',
-    //         printChart: 'Imprimer le graphique',
-    //         downloadPNG: 'Télécharger en PNG',
-    //         downloadJPEG: 'Télécharger en JPEG',
-    //         downloadPDF: 'Télécharger en PDF',
-    //         downloadSVG: 'Télécharger en SVG',
-    //         downloadCSV: 'Exporter en CSV',
-    //         downloadXLS: 'Exporter en XLS',
-    //         viewData: 'Voir la table de données',
-    //         contextButtonTitle: 'Menu contextuel'
-    //     },
-
-    //     navigation: {
-    //         buttonOptions: {
-    //             theme: {
-    //                 // Good old text links
-    //                 // todo
-    //                 style: {
-    //                     color: '#039',
-    //                     textDecoration: 'underline'
-    //                 }
-    //             }
-    //         }
-    //     },
-
-    //     exporting: {
-    //         buttons: {
-    //             contextButton: {
-    //                 enabled: true
-    //             },
-    //             exportButton: {
-    //                 text: 'Télécharger',
-    //                 // Use only the download related menu items from the default
-    //                 // context button
-    //                 menuItems: [
-    //                     'downloadPNG',
-    //                     'downloadJPEG',
-    //                     'separator',
-    //                     'downloadCSV'
-    //                 ]
-    //             }
-    //         }
-    //     },
-
-    //     title: {
-    //         text: `Évolution de la ${units.name} toute les 30 minutes`
-    //     },
-
-    //     yAxis: {
-    //         title: {
-    //             text: `Y axis`
-    //         }
-    //     },
-
-    //     xAxis: {
-    //         title: {
-    //             text: 'Date de X à Y FAIRE L\'DU TEMPS'
-    //         }
-    //     },
-
-    //     plotOptions: {
-    //         series: {
-    //             label: {
-    //                 connectorAllowed: false
-    //             },
-    //             pointStart: 2010
-    //         }
-    //     },
-
-    //     legend: {
-    //         align: 'left',
-    //         verticalAlign: 'top',
-    //         borderWidth: 0
-    //     },
-
-    //     series: [{
-    //         // yAxis: 0,
-    //         // type: 'line',
-    //         // name: 'Température',
-    //         data: {
-    //             x: dataPoints,
-    //             y: dataPointsX
-    //         }
-    //     }
-    //     ],
-
-    //     tooltip: {
-    //         dateTimeLabelFormats: {
-    //             hour: '%A, %b %e, %l %p'
-    //         },
-    //     },
-    //     xAxis: {
-    //         dateTimeLabelFormats: {
-    //             hour: '%l %p'
-    //         },
-    //         type: 'datetime'
-    //     },
-
-    //     // caption: {
-    //     //     text: 'Temperature'
-    //     // },
-
-    //     // chart: {
-    //     // backgroundColor: 'gray'
-    //     // https://api.highcharts.com/highcharts/chart
-    //     // },
-
-
-
-    //     // loading: {
-    //     //     // https://api.highcharts.com/highcharts/loading
-    //     // }
-
-    //     // tooltip: {
-    //     //     // https://api.highcharts.com/highcharts/tooltip
-    //     // },
-
-    //     // responsive: {
-    //     // https://api.highcharts.com/highcharts/responsive
-    //     //     rules: [{
-    //     //         condition: {
-    //     //             maxWidth: 500
-    //     //         },
-    //     //         chartOptions: {
-    //     //             legend: {
-    //     //                 layout: 'horizontal',
-    //     //                 align: 'center',
-    //     //                 verticalAlign: 'bottom'
-    //     //             }
-    //     //         }
-    //     //     }]
-    //     // }
-    // });
-    //#endregion
+    console.timeEnd('Drawing chart in')
 }
 
 (async () => {
-    // Getting balise id
-    const b_id = window.location.search.split('?')[1].split('=')[1]
-    console.log({ b_id });
+    _toast = new Toast($('toast-container'))
 
-    // Fetching balise (node) data
+    // Récupération de l'id de la balise depuis l'url (?balise_id=)
+    const b_id = window.location.search.split('?')[1].split('=')[1]
+    console.log("window b_id:", b_id);
+
+    // Récupération des données de la balise
     const b_data = await fetchNode(b_id)
     console.log('b_data', b_data);
 
-    // Getting sensors id
-    const sensors_list = b_data.sensors_id.split(',')
-    console.log('sensors_list', sensors_list);
+    if (!b_data) {
+        _toast.show("", "Aucune donnée disponible pour cette balise", 'info', {
+            autohide: false
+        })
+        $('chartsContainer').innerHTML += '<span class="no-data">Aucune donnée disponible pour cette balise</span>'
+    } else {
+        // Récupération des capteurs de la balise
+        const sensors_list = b_data.sensors_id.split(',')
+        console.log('sensors_list', sensors_list);
 
-    // Fetching sensors data
-    var sensors_data = []
-    var sensors_units = []
-
-
-    console.time('fetchValues')
-    const asyncForEach = async (array, callback) => {
-        for (let index = 0; index < array.length; index++) {
-            await callback(array[index], index, array);
+        const asyncForEach = async (array, callback) => {
+            for (let index = 0; index < array.length; index++) {
+                await callback(array[index], index, array);
+            }
         }
+
+        await asyncForEach(sensors_list, async (sensor_id) => {
+            // Récupération des valeur à affichier
+            var s_data = await fetchValues(sensor_id);
+            // if (s_data.length >= 1) {
+            let sensors_units = { name: s_data[0].name, unit: s_data[0].unit, symb: s_data[0].symbol }
+            let sensors_data = s_data
+
+            // Debug
+            console.debug("sensors_data:", sensors_data);
+            console.debug("sensors_units:", sensors_units);
+
+            // Création du container
+            var container = document.createElement('div')
+            container.setAttribute('id', `chartContainer${sensor_id}`)
+            $('chartsContainer').appendChild(container)
+            $(`chartContainer${sensor_id}`).classList += 'chart';
+
+            // Drawing chart
+            drawChart(sensors_data, sensors_units, `chartContainer${sensor_id}`)
+            // } else {
+            //     _toast.show("error", "Aucune donnée disponible pour ce capteur)")
+            // }
+        });
+
+        $('battery').innerHTML += b_data.battery_level + '%'
+
+        $('name').innerHTML = b_data.name
+
     }
-    await asyncForEach(sensors_list, async (sensor_id) => {
-        let s_data = await fetchValues(sensor_id)
-        console.timeLog('fetchValues', 'fetched sensor data')
-        sensors_units.push({ name: s_data[0].name, unit: s_data[0].unit, symb: s_data[0].symbol })
-        sensors_data.push(s_data)
-        // id, balise_id, sensor_id, value, timestamp
-    });
-    console.timeEnd('fetchValues')
 
-
-    console.log(sensors_data);
-    console.log(sensors_units);
-
-    // Draw Charts
-    drawChart(sensors_data, sensors_units, 'chartContainer')
-
-    $('battery').innerHTML += b_data.battery_level + '%'
-    $('name').innerHTML = b_data.name
 })()
