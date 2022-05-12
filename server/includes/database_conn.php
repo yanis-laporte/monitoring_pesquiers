8057dabef3541a200d8c6d1b71210347d403bc57
Yanis <yanislapo@gmail.com>
2022-05-12 11:19:03 +0200

    typo

--- server/includes/database_conn.php
+++ server/includes/database_conn.php
@@ -2,7 +2,9 @@
 date_default_timezone_set('Europe/Paris');
 
 try {
-    $bdd = new PDO('mysql:host=db;dbname=monitoring_pesquiers;charset=utf8', 'root', 'root');
+    // $bdd = new PDO('mysql:host=db;dbname=monitoring_pesquiers;charset=utf8', 'root', 'root');
+    // $bdd = new PDO('mysql:host=salins.btssnir.lycee-costebelle.fr;dbname=monitoring_pesquiers;charset=utf8', 'py_con', 'pesquierPY');
+    $bdd = new PDO('mysql:host=192.168.0.120;dbname=monitoring_pesquiers;charset=utf8', 'py_con', 'pesquierPY');
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
