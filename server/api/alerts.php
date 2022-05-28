dcc083899783a409915e793c342f86c9c8302271
Yanis <yanislapo@gmail.com>
2022-05-28 15:09:10 +0200

    update api

--- server/api/alerts.php
+++ server/api/alerts.php
@@ -9,6 +9,7 @@ include('../includes/functions.php');
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $i = 0;
     $req = $bdd->query('SELECT * FROM alerts');
+    $res = [];
     while ($req_f = $req->fetch(PDO::FETCH_ASSOC)) {
         foreach ($req_f as $key => $value) {
             $res[$i][$key] = $value;
@@ -29,21 +30,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // Vérifie si les bonnes données sont envoyées
     try {
-        issetArray($_POST, ['name', 'balise_id', 'sensor_id', 'control', 'sign', 'email']);
+        issetArray($_POST, ['name', 'balise_id', 'sensor_id', 'control', 'sign']);
     } catch (\Throwable $th) {
         res(array(
             "error" => $th->getMessage()
         ), 400);
     }
 
-    $req = $bdd->prepare('INSERT INTO alerts (name, balise_id, sensor_id, control, sign, email) VALUES (:name, :balise_id, :sensor_id, :control, :sign, :email)');
+    $req = $bdd->prepare('INSERT INTO
+        alerts (name, balise_id, sensor_id, control, sign)
+        VALUES (:name, :balise_id, :sensor_id, :control, :sign)');
     $req->execute(array(
         "name" => $_POST['name'],
         "balise_id" => $_POST['balise_id'],
         "sensor_id" => $_POST['sensor_id'],
         "control" => $_POST['control'],
-        "sign" => $_POST['sign'],
-        "email" => $_POST['email']
+        "sign" => $_POST['sign']
     ));
 
     // Réponse
@@ -59,30 +61,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'PATCH') {
 
     // Vérifie si les bonnes données sont envoyées
     try {
-        issetArray($_PATCH, ['name', 'balise_id', 'sensor_id', 'control', 'sign', 'email', 'alert_id']);
+        issetArray($_PATCH, ['alert_id', 'name', 'balise_id', 'sensor_id', 'control', 'sign']);
     } catch (\Throwable $th) {
         res(array(
             "error" => $th->getMessage()
         ), 400);
     }
 
-
     $req = $bdd->prepare('UPDATE alerts SET
         name = :name,
         balise_id = :balise_id,
-        sensor_id =  :sensor_id,
-        control =  :control,
-        sign =  :sign,
-        email =  :email
-        WHERE alert_id = :alert_id;');
+        sensor_id = :sensor_id,
+        control = :control,
+        sign = :sign
+        WHERE alert_id = :alert_id');
     $req->execute(array(
         "alert_id" => $_PATCH['alert_id'],
         "name" => $_PATCH['name'],
         "balise_id" => $_PATCH['balise_id'],
         "sensor_id" => $_PATCH['sensor_id'],
         "control" => $_PATCH['control'],
-        "sign" => $_PATCH['sign'],
-        "email" => $_PATCH['email']
+        "sign" => $_PATCH['sign']
     ));
 
     // Réponse
