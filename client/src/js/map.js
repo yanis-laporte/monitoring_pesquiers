416a88d8af7f2119a0671dc6299c7fe1a52375c4
Yanis <yanislapo@gmail.com>
2022-05-28 15:10:39 +0200

    refonte

--- client/src/js/map.js
+++ client/src/js/map.js
@@ -1,6 +1,6 @@
 import { API_URL } from './base'
 
-var map = L.map('map', { doubleClickZoom: false }).setView([43.05669098943963, 6.140842437744141], 14);
+var map = L.map('map', { doubleClickZoom: false }).setView([43.05916816778099, 6.140842437744141], 14);
 
 L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
     attribution:
@@ -9,22 +9,28 @@ L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
     maxZoom: 18
 }).addTo(map);
 
-//fetch data from api
+// Récuprération des données des balises
 fetch(`${API_URL}/nodes.php`, {
     mode: 'cors',
 })
     .then(res => res.json())
     .then(data => {
-        console.debug("list balise: ", data);
         data.forEach((balise) => {
-            // console.debug(balise);
-            L.marker([balise.latitude, balise.longitude], { title: "Balise", balise_id: Number(balise.balise_id), draggable: true })
+            console.debug('marker', balise);
+            // Définition des markers
+            L.marker(
+                [balise.latitude, balise.longitude],
+                {
+                    title: "Balise",
+                    balise_id: Number(balise.balise_id),
+                    draggable: false
+                })
                 .on('click', (e) => {
                     window.open('./charts.html?balise_id=' + e.target.options.balise_id, '_blank');
                 })
-                .on('moveend', (e) => {
-                    console.log(balise.name, e.sourceTarget._latlng);
-                })
+                // .on('moveend', (e) => {
+                //     console.log(balise.name, e.sourceTarget._latlng);
+                // })
                 .bindTooltip(`${balise.name} | 🔋${balise.battery_level}%`, { permanent: false, direction: 'top' })
                 .addTo(map);
         }
